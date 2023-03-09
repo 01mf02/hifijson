83639bcc8613bd39ecb8332ad5ef70027e86c986
Michael Färber <01mf02@gmail.com>
2023-03-09 10:55:39 +0100

    Documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,99 @@
 //! High-fidelity JSON lexer and parser.
 //!
+//!
+//! # Introduction
+//!
+//! JSON is a data format that is underspecified and sometimes contradictory.
+//! As reference, I recommend the excellent article "[Parsing JSON is a Minefield]".
+//! In particular, it is ambiguous how to parse strings and numbers.
+//! For example, JSON does not impose any restriction on the maximal size of numbers,
+//! but in reality, most JSON parsers use a lossless representation,
+//! for example 64-bit floating point.
+//! This is allowed by the JSON specification; however,
+//! at the same time, if we are allowed to fix arbitrariy maximal sizes,
+//! then a parser that fails on every input is a valid parser!
+//! I hope that I could convince you at this point that this is all quite a mess.
+//! The best I can do to help you around this mess is to give *you*
+//! a tool to deal with this mess in the way that suits you most.
+//! hifijson is this tool.
+//!
+//! [Parsing JSON is a Minefield]: http://seriot.ch/projects/parsing_json.html
+//!
+//! What makes hifijson so flexible is that unlike most other JSON parsers,
+//! it exposes its basic building blocks, called [*lexers*](#lexers),
+//! that allow you to build your own parsers on top of them.
+//!
+//! Because hifijson exposes a variety of lexers and parsers,
+//! you can combine them in a way that allows you to achieve your desired behaviour,
+//! without having to write everything from scratch.
+//! For example, suppose that your input data does not contain escape sequences (`\n`, `\uxxxx`);
+//! then you can use the [`str::LexWrite::str_bytes`] function that is
+//! guaranteed to never allocate memory when lexing from a slice,
+//! making it suitable for usage in embedded environments.
+//! Or suppose that you are reading an object `{"title": ..., "reviews": ...}`,
+//! and you do not feel like caring about reviews today.
+//! Then you can simply skip reading the value for reviews by using [`ignore::parse`].
+//! Or, if you just want to have a complete JSON value, then
+//! you can use [`value::parse_unbounded`].
+//! The choice is yours.
+//!
+//! In summary, hifijson aims to give you the tools to interpret JSON data
+//! flexibly and performantly.
+//!
+//! ## Lexers
+//!
+//! A *lexer* is a program that breaks up input into small units, and
+//! a parser combines these small units to transform them into the desired shape.
+//! For example, consider the JSON input `[null, {}]`.
+//! A lexer could break these into the units `[`, `null`, `,`, `{`, `}`, and `]`.
+//!
+//! JSON parsing consists of about three mostly independent lexing tasks:
+//! strings, numbers, and everything else (in decreasing order of difficulty).
+//! The "everything else" part is what I call a [token](Token).
+//! That includes:
+//! * `[`, `]` (start and end of an array)
+//! * `{`, `}` (start and end of an object)
+//! * `,`
+//! * `:`
+//! * `null`
+//! * `true`
+//! * `false`
+//! * `"` (start of a string)
+//! * `-` and any digit from 0 to 9 (start of a number)
+//!
+//! So every token has a maximal length (namely length 5).
+//!
+//! What about strings and numbers?
+//! The token lexer is not responsible for lexing them, but it
+//! returns special tokens to indicate that it has encountered the *start* of a string or number.
+//! Once that we get such a token, we can use one of the many string/number lexers,
+//! which differ most prominently in their memory allocation behaviour.
+//! For example,
+//! * [`str::Lex::str_ignore`] discards a string,
+//! * [`str::LexWrite::str_bytes`] reads a string, but does not interpret escape sequences, and
+//! * [`str::LexAlloc::str_string`] reads a string and interprets escape sequences.
+//!
+//! In particular,
+//! lexers that implement the [`Lex`] trait do *never* allocate memory;
+//! lexers that implement the [`LexWrite`] trait only allocate memory when lexing from iterators,
+//! and
+//! lexers that implement the [`LexAlloc`] trait may allocate memory when lexing from both
+//! iterators and slices.
+//!
+//! ## Slices and Iterators
+//!
+//! One important feature of hifijson is that it allows to read from both
+//! [slices](SliceLexer) and [iterators](IterLexer) over bytes.
+//! This is useful when your application should support reading from both
+//! files and streams (such as standard input).
+//!
+//! ## Feature Flags
+//!
+//! If you build hifijson without the feature flag `alloc`, you disable any allocation.
+//! If you build hifijson with the feature flag `serde`,
+//! then you can use hifijson to deserialise JSON to data types implementing `serde::Deserialize`.
+//!
+//!
 //! # Examples
 //!
 //! ## Parsing strings to values
@@ -34,9 +128,6 @@
 //!
 //! ## Parsing files and streams
 //!
-//! One important feature of hifijson is that it allows to read from both slices and iterators.
-//! This is useful when your application should support reading from both files and standard input.
-//!
 //! The following example reads JSON from a file if an argument is given,
 //! otherwise from standard input:
 //!
@@ -60,9 +151,12 @@
 //! }
 //! ~~~
 //!
+//! We just made a pretty printer (stretching the definition of pretty pretty far).
+//!
 //! ## Operating on the lexer
 //!
-//! Often, it is better for performance to operate directly on the tokens that the lexer yields.
+//! Often, it is better for performance to operate directly on the tokens that the lexer yields
+//! rather than parsing everything into a value and then processing the value.
 //! For example, the following example counts the number of values in the input JSON.
 //! Unlike the previous examples, it requires only constant memory!
 //!
@@ -89,7 +183,7 @@
 //!             // perform the following for every item of the array
 //!             lexer.seq(Token::RSquare, |token, lexer| {
 //!                 sum += count(token, lexer)?;
-//!                 Ok::<_, hifijson::Error>(())
+//!                 Ok::<_, Error>(())
 //!             })?;
 //!             Ok(sum)
 //!         }
@@ -104,7 +198,7 @@
 //!                 /// now read the token after ':'
 //!                 let token = lexer.ws_token().ok_or(hifijson::Expect::Value)?;
 //!                 sum += count(token, lexer)?;
-//!                 Ok::<_, hifijson::Error>(())
+//!                 Ok::<_, Error>(())
 //!             })?;
 //!             Ok(sum)
 //!         }
@@ -121,6 +215,11 @@
 //! let n = process(lexer).unwrap();
 //! assert_eq!(n, 13)
 //! ~~~
+//!
+//! ## More Examples
+//!
+//! See the [cat](examples/cat.rs) program for a more worked version of a JSON "pretty" printer
+//! that can be also used to filter parts of the data based on a path.
 
 #![no_std]
 #![forbid(unsafe_code)]
